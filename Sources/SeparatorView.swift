9878dd887bb960218418ec623102c8bceba5f615
Mikhail <Mikhail@Mikhails-MacBook-Pro.local>
2018-12-21 23:15:32 +0700

    separator view

---
+++ Sources/SeparatorView.swift
@@ -0,0 +1,43 @@
+//
+//  SeparatorView.swift
+//  SwiftToolkit
+//
+//  Created by Mikhail on 21/12/2018.
+//  Copyright © 2018 glvrzzz. All rights reserved.
+//
+
+import UIKit
+
+class SeparatorView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    internal func configure() {
+        isUserInteractionEnabled = false
+        setContentHuggingPriority(.required, for: .horizontal)
+        setContentHuggingPriority(.required, for: .vertical)
+        setContentCompressionResistancePriority(.required, for: .horizontal)
+        setContentCompressionResistancePriority(.required, for: .vertical)
+    }
+
+    final var thickness: CGFloat = 1 { // in pixels
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+    
+    override var intrinsicContentSize : CGSize {
+        let scale = traitCollection.displayScale
+        let pixel = thickness / scale
+        return CGSize(width: pixel, height: pixel)
+    }
+    
+}
