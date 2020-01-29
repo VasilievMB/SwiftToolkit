16fb97fce370c336143bed204083814e1ac927b9
Mikhail <glvrzzz@gmail.com>
2020-01-29 21:33:50 +0700

    UIImage with solid color

---
+++ Sources/UIImage+SolidColor.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+SolidColor.swift
+//  SwiftToolkit
+//
+//  Created by Mikhail on 29.01.2020.
+//  Copyright © 2020 glvrzzz. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+    
+    class func imageWithSolidColor(_ color: UIColor,
+                     size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+
+}
