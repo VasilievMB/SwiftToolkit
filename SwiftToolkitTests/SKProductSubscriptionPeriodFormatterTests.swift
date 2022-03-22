0e70b0a5f4b102e7bb90900a8ab5c4b7540a03e4
Mikhail <3124667+VasilievMB@users.noreply.github.com>
2022-03-22 08:18:45 +0000

    SKProductSubscriptionPeriodFormatterTests updated

--- SwiftToolkitTests/SKProductSubscriptionPeriodFormatterTests.swift
+++ SwiftToolkitTests/SKProductSubscriptionPeriodFormatterTests.swift
@@ -10,18 +10,30 @@ import XCTest
 import StoreKit
 @testable import SwiftToolkit
 
+private extension SKProductSubscriptionPeriod {
+    
+    convenience init(numberOfUnits: Int, unit: SKProduct.PeriodUnit) {
+        self.init()
+        setValue(NSNumber(value: numberOfUnits), forKey: "numberOfUnits")
+        setValue(unit.rawValue, forKey: "unit")
+    }
+    
+}
+
 class SKProductSubscriptionPeriodFormatterTests: XCTestCase {
 
     func testFormatting() throws {
         let formatter = SKProductSubscriptionPeriodFormatter()
         formatter.calendar?.locale = Locale(identifier: "en_US")
         
-        let period = SKProductSubscriptionPeriod()
-        XCTAssertEqual(period.numberOfUnits, 0)
-        XCTAssertEqual(period.unit, .day)
-        
-        let string = try XCTUnwrap(formatter.string(from: period))
-        XCTAssertEqual(string, "0 days")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 1, unit: .day)), "1 day")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 2, unit: .day)), "2 days")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 1, unit: .week)), "1 week")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 3, unit: .week)), "3 weeks")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 1, unit: .month)), "1 month")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 6, unit: .month)), "6 months")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 1, unit: .year)), "1 year")
+        XCTAssertEqual(formatter.string(from: SKProductSubscriptionPeriod(numberOfUnits: 2, unit: .year)), "2 years")
     }
 
 }
